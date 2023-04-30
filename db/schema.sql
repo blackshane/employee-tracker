7053ec49701f0d3fe53ad3d04779ef6ec3bd4093
blackshane <122633142+blackshane@users.noreply.github.com>
2023-04-29 21:03:04 -0500

    tables and seed works properly

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,34 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-),
+);
 
 CREATE TABLE role (
-    id AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    salary DECIMAL(10) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
-),
+        REFERENCES department(id)
+        ON DELETE SET NULL
+);
 
 CREATE TABLE employee (
-    id AUTO_INCREMENT NOT NULL,
+    id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    
-    )
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL
+    );
 
