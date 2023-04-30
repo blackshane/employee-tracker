ae14daed99a12159e328826063ca98914f5a8ce8
blackshane <122633142+blackshane@users.noreply.github.com>
2023-04-30 18:08:35 -0500

    setup .env connection

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+const config = dotenv.config();
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
+
 const db = mysql.createConnection(
     {
     host: 'localhost',
-    user: DB_NAME,
-    password: DB_PASSWORD,
-    database: DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     },
 );
+
+// Include how to setup .env in README.
+
+db.connect(function(error) {
+    if(error) throw error;
+    console.log(`Connected at port ${PORT}`); 
+})
