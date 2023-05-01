2d85879a914abbaf332af2365834ada8cb3670f2
blackshane <122633142+blackshane@users.noreply.github.com>
2023-05-01 09:54:39 -0500

    set up inquirer prompt

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const dotenv = require('dotenv');
 const config = dotenv.config();
 const inquirer = require('inquirer');
 
+// Initiate express and establish PORT
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 
+// Create connection
 const db = mysql.createConnection(
     {
     host: 'localhost',
@@ -23,4 +25,90 @@ const db = mysql.createConnection(
 db.connect(function(error) {
     if(error) throw error;
     console.log(`Connected at port ${PORT}`); 
-})
+});
+
+// Prompt User for Choices
+const promptUser = () => {
+  inquirer.prompt([
+      {
+        name: 'choices',
+        type: 'list',
+        message: 'Please select an option:',
+        choices: [
+          'View All Employees',
+          'View All Roles',
+          'View All Departments',
+          'View All Employees By Department',
+          'View Department Budgets',
+          'Update Employee Role',
+          'Update Employee Manager',
+          'Add Employee',
+          'Add Role',
+          'Add Department',
+          'Remove Employee',
+          'Remove Role',
+          'Remove Department',
+          'Exit'
+          ]
+      }
+    ])
+    .then((answers) => {
+      const {choices} = answers;
+
+        if (choices === 'View All Employees') {
+            viewAllEmployees();
+        }
+
+        if (choices === 'View All Departments') {
+          viewAllDepartments();
+      }
+
+        if (choices === 'View All Employees By Department') {
+            viewEmployeesByDepartment();
+        }
+
+        if (choices === 'Add Employee') {
+            addEmployee();
+        }
+
+        if (choices === 'Remove Employee') {
+            removeEmployee();
+        }
+
+        if (choices === 'Update Employee Role') {
+            updateEmployeeRole();
+        }
+
+        if (choices === 'Update Employee Manager') {
+            updateEmployeeManager();
+        }
+
+        if (choices === 'View All Roles') {
+            viewAllRoles();
+        }
+
+        if (choices === 'Add Role') {
+            addRole();
+        }
+
+        if (choices === 'Remove Role') {
+            removeRole();
+        }
+
+        if (choices === 'Add Department') {
+            addDepartment();
+        }
+
+        if (choices === 'View Department Budgets') {
+            viewDepartmentBudget();
+        }
+
+        if (choices === 'Remove Department') {
+            removeDepartment();
+        }
+
+        if (choices === 'Exit') {
+            connection.end();
+        }
+  });
+};
